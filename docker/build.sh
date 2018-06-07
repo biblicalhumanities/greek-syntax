7abf6e7696e8e21149fa5010fc30ec75c99c122b
Jonathan Robie <jonathan.robie@biblicalhumanities.org>
2018-06-07 18:58:10 -0400

    More structure around the Docker project.

---
+++ docker/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -e github ]; then
+	echo "Using existing local repositories"
+else
+	mkdir github
+	pushd github
+	git clone https://github.com/biblicalhumanities/Nestle1904
+	git clone https://github.com/biblicalhumanities/greek-new-testament
+	git clone https://github.com/biblicalhumanities/levinsohn
+	git clone https://github.com/translatable-exegetical-tools/Abbott-Smith
+	popd
+fi
+
+docker build - < Dockerfile
