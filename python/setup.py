d041bb1095f1de5f92ade154e68604a5a0aa7c5d
Jonathan Robie <jonathan.robie@biblicalhumanities.org>
2018-06-08 11:32:37 -0400

    Adding setup.py to allow easy installation on the host machine.

---
+++ python/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name = "greeksyntax",
+    version = "0.1dev",
+    license = "Apache 2.0",
+    author = "Jonathan Robie",
+    author_email = "jonathan.robie@biblicalhumanities.org",
+    py_modules = ["greeksyntax"],
+)
+
