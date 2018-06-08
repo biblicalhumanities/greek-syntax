bbebfd8986199435f1542b8685087faeb151c961
Jonathan Robie <jonathan.robie@biblicalhumanities.org>
2018-06-08 12:01:18 -0400

    Fixed bug - it installs correctly now.

--- python/setup.py
+++ python/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name = "greeksyntax",
-    version = "0.1dev",
+    version = "0.1.dev0",
     license = "Apache 2.0",
     author = "Jonathan Robie",
     author_email = "jonathan.robie@biblicalhumanities.org",
-    py_modules = ["greeksyntax"],
+    packages = ["greeksyntax"],
 )
 
